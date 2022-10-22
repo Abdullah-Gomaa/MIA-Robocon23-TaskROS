70b50ee0c0abd085e918be80e21a46400fde115d
Abdullah-Gomaa <111183924+Abdullah-Gomaa@users.noreply.github.com>
2022-10-22 20:36:13 +0200

    Use dmp library

--- YawAngleROS.ino
+++ YawAngleROS.ino
@@ -1,12 +1,33 @@
 #include <ros.h>
 #include <std_msgs/Float32.h>
-#include "I2Cdev.h"
-#include "MPU6050.h"
-#include "Wire.h"
+#include <I2Cdev.h>
+#include <MPU6050_6Axis_MotionApps20.h>
 
-#define NoOfSamples             1000
-#define gyroFullScaleRange      MPU6050_GYRO_FS_2000
 
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+MPU6050 mpu;
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+float YawAngle = 0.0;
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 /*****************************************************************************************************************************************/
 // Create an instance of MPU6050
   MPU6050 gyro;
@@ -21,17 +42,9 @@
 // Inistantiate publisher with topic name YawAngle 
   ros :: Publisher pub_MPU6050("YawAngle", &msg);
 /*****************************************************************************************************************************************/
-// Variables for angle calibration and calculations
-  int16_t gyroZ = 0;
-  int16_t gyroZOffset = 0;
-  float YawAngle = 0.0;
-  unsigned long prevT = 0;
-  unsigned long currentT = 0;
-  float deltaT = 0.0;
-/*****************************************************************************************************************************************/
 // Functions Prototypes
-  void MPU6050_CalculateOffsetGyroZ();
   void MPU6050_CalculateYawAngle();
+  void setupDMP();
 /*****************************************************************************************************************************************/
 void setup() {
   
@@ -40,18 +53,7 @@ void setup() {
 // Advertise to the topic published by pub_MPU6050  
   node.advertise(pub_MPU6050);
 
-// Begin I2C communitcation
-  Wire.begin();
-  
-// Initialize gyro
-  gyro.initialize();
-  
-// Set Full Scale Range -2000 ---> +2000 deg/s  
-  gyro.setFullScaleGyroRange(gyroFullScaleRange);
-
-// Calculate Offset for gyroZ  
-  MPU6050_CalculateOffsetGyroZ();
-  delay(5);
+  setupDMP();
 }
 /*****************************************************************************************************************************************/
 void loop() {
@@ -69,26 +71,66 @@ void loop() {
   node.spinOnce();
   delay(100);
 }
-/*****************************************************************************************************************************************/
-void MPU6050_CalculateOffsetGyroZ()
-{
-  for(int i=0; i< NoOfSamples; i++)
-  {
-    gyroZ = gyro.getRotationZ();
-    gyroZOffset += gyroZ; 
-  }
-  gyroZOffset /= NoOfSamples;
-}
+
 /*****************************************************************************************************************************************/
 void MPU6050_CalculateYawAngle()
 {
-  // Time measurements to calculate angle
-  prevT =  currentT;
-  currentT = millis();
-  deltaT = (currentT - prevT)*1000;
-  // Reading the gyroZ register value
-  gyroZ = gyro.getRotationZ();
-  // Calculate yaw angle
-  YawAngle = ((gyroZ * gyroFullScaleRange ) / 32786)*deltaT;
+  if (!dmpReady) return;
+  #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetCurrentFIFOPacket(fifoBuffer);
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI);
+            YawAngle = ypr[0] * 180/M_PI;
+            
+        #endif
 }
 /*****************************************************************************************************************************************/
+void setupDMP()
+{
+  // put your setup code here, to run once:
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    Serial.begin(57600);
+    while (!Serial);
+    mpu.initialize();
+     devStatus = mpu.dmpInitialize();
+      // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+}
